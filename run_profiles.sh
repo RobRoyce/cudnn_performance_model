b12fb7bc699df50d5731460d1024c82337f9c24e
Rob Royce <robertris2@gmail.com>
2021-05-16 15:29:37 -0700

    Added GEMM training and inference with feature importance.

--- run_profiles.sh
+++ run_profiles.sh
@@ -1,6 +1,6 @@
-DEEPBENCH_BIN=/home/rob/Developer/ucla/cs259/DeepBench/code/nvidia/bin
-CUDNN_CONV_PARAMS=/home/rob/Developer/ucla/cs259/cudnn_performance_model/config/cudnn_conv_params.csv
-CUDNN_GEMM_PARAMS=/home/rob/Developer/ucla/cs259/cudnn_performance_model/config/cudnn_gemm_params.csv
+DEEPBENCH_BIN=/usr/eda/cs259/robroyce/DeepBench/code/nvidia/bin
+CUDNN_CONV_PARAMS=/usr/eda/cs259/robroyce/cudnn_performance_model/config/cudnn_conv_params.csv
+CUDNN_GEMM_PARAMS=/usr/eda/cs259/robroyce/cudnn_performance_model/config/cudnn_gemm_params.csv
 
 cd $DEEPBENCH_BIN
 
@@ -9,6 +9,9 @@ echo "gemm_bench inference float"
 
 echo "gemm_bench inference half"
 ./gemm_bench inference half $CUDNN_GEMM_PARAMS > gemm_inference_half
+
+echo "gemm_bench inference int8"
+./gemm_bench inference int8 $CUDNN_GEMM_PARAMS > gemm_inference_int8
 
 echo "conv_bench inference float"
 ./conv_bench inference float $CUDNN_CONV_PARAMS > conv_inference_float
